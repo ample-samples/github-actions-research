7e9910c43574f12ca93dd66a95557c6cb4eae89b
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-08 14:22:16 +0100

    Twosum tests.

--- github-actions/src/tests/twoSum/twoSum.ts
+++ github-actions/src/tests/twoSum/twoSum.ts
@@ -1,6 +1,5 @@
-const twoSum = (nums: number[], target: number) => {
+export const twoSum = (nums: number[], target: number) => {
   const map = new Map();
-  //value:index
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
     if (map.has(complement)) {
@@ -10,5 +9,3 @@ const twoSum = (nums: number[], target: number) => {
     }
   }
 };
-
-console.log(twoSum([2, 5, 7, 10], 9));
